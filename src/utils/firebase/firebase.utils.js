58283ad681d63ae8b3e16012a5220617b35691e7
Daisy Le <daisy.le0203@gmail.com>
2022-12-10 23:35:38 -0600

    add sign up form with email and password

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "firebase/app"
 import {
   getAuth,
-  signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from "firebase/auth"
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore"
 
@@ -22,7 +22,7 @@ const firebaseApp = initializeApp(firebaseConfig)
 const provider = new GoogleAuthProvider()
 
 provider.setCustomParameters({
-    prompt: "select_account"
+  prompt: "select_account",
 })
 
 export const auth = getAuth()
@@ -30,37 +30,41 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
 
 export const db = getFirestore()
 
-export const createUserDocumentFromAuth = async (userAuth) => {
-    const userDocRef = doc(db, "users", userAuth.uid)
+export const createUserDocumentFromAuth = async (userAuth, additionalInfo = {}) => {
+  if (!userAuth) return
+  const userDocRef = doc(db, "users", userAuth.uid)
 
-    console.log(userDocRef);
+  console.log(userDocRef)
 
-    const userSnapshot = await getDoc(userDocRef)
-    console.log(userSnapshot);
-    console.log(userSnapshot.exists());
-    
-    // if user data does not exist, set the user in the database
-    // return userDocRef
-    if(!userSnapshot.exists()) {
-      const { displayName, email } = userAuth
-      const createdAt = new Date()
+  const userSnapshot = await getDoc(userDocRef)
+  console.log(userSnapshot)
+  console.log(userSnapshot.exists())
 
-      try {
-        await setDoc(userDocRef, {
-          displayName,
-          email,
-          createdAt
-        })
-      } catch (error) {
-        console.log("error creating the user", error.message);
-      }
-    }
-
-    return userDocRef
+  // if user data does not exist, set the user in the database
+  // return userDocRef
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
 
     // if user data exists
     // create/ set the document with the data from userAuth in my collection
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInfo
+      })
+    } catch (error) {
+      console.log("error creating the user", error.message)
+    }
+  }
 
-    
+  return userDocRef
 }
 
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return
+
+  return await createUserWithEmailAndPassword(auth, email, password)
+}
