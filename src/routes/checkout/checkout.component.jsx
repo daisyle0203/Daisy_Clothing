c2bc88efaf493a4d90006d15ca84965d2206aa38
Daisy Le <daisy.le0203@gmail.com>
2022-12-12 04:22:05 -0600

    add checkout item component and styling for it

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react"
 
 import { CartContext } from "../../contexts/cart.context"
 
+import CheckoutItem from "../../components/checkout-item/checkout-item.component"
+
 import "./checkout.styles.scss"
 
 const Checkout = () => {
@@ -27,22 +29,12 @@ const Checkout = () => {
           <span>Remove</span>
         </div>
       </div>
-      {cartItems.map((cartItem) => {
-        const { id, name, quantity } = cartItem
-        return (
-          <div key={id}>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
-            <br />
-            <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
-            <br />
-            <span onClick={() => addItemToCart(cartItem)}>increment</span>
-          </div>
-        )
-      })}
-      <span className="Total">0</span>
+      {cartItems.map((cartItem) => <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      )}
+      <span className="total">Total: 0</span>
     </div>
   )
 }
 
 export default Checkout
+  
