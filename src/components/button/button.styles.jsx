4669533f7f48d5f25311decce0f4d0c5547c3329
Daisy Le <daisy.le0203@gmail.com>
2022-12-13 14:15:31 -0600

    adding styled-components for button

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,4 +1,6 @@
-.button-container {
+import styled from "styled-components"
+
+export const BaseButton = styled.button`
   min-width: 165px;
   width: auto;
   height: 50px;
@@ -22,27 +24,27 @@
     color: black;
     border: 2px solid black;
   }
+`
 
-  &.google-sign-in {
-    background-color: rgba(255, 255, 0, 0.709);
-    color: black;
-    border: 2px solid black;
+export const GoogleSignInButton = styled(BaseButton)`
+  background-color: rgba(255, 255, 0, 0.709);
+  color: black;
+  border: 2px solid black;
 
-    &:hover {
-      background-color: white;
-      border: 2px solid black;
-    }
-  }
-
-  &.inverted {
+  &:hover {
     background-color: white;
+    border: 2px solid black;
+  }
+`
+
+export const InvertedButton = styled(BaseButton)`
+  background-color: white;
+  color: black;
+  border: 2px solid black;
+
+  &:hover {
+    background-color: rgb(12, 182, 228);
     color: black;
     border: 2px solid black;
-
-    &:hover {
-      background-color: rgb(12, 182, 228);
-      color: black;
-      border: 2px solid black;
-    }
   }
-}
+`
