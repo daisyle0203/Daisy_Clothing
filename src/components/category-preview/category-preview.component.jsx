265bf600a27c0385dbbfb7e6b40fe539d250b052
Daisy Le <daisy.le0203@gmail.com>
2022-12-13 00:09:46 -0600

    turn the category title to link

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 import ProductCard from "../product-card/product-card.component"
 
 import "./category-preview.styles.scss"
@@ -5,15 +7,18 @@ import "./category-preview.styles.scss"
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
-        <h2>
-            <span className="title">{title.toUpperCase()}</span>
-        </h2>
-        <div className="preview">
-            {
-                products.filter((_, index) => index < 4)
-                .map((product) => <ProductCard  key={product.id} product={product}/>)
-            }
-        </div>
+      <h2>
+        <Link className="title" to={title}>
+          {title.toUpperCase()}
+        </Link>
+      </h2>
+      <div className="preview">
+        {products
+          .filter((_, index) => index < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
     </div>
   )
 }
