9881cde846ea91399d31d1e2cff2ba828c113f75
Daisy Le <daisy.le0203@gmail.com>
2022-12-13 23:37:58 -0600

    add styled components for category component

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,29 +2,29 @@ import { useContext, useState, useEffect, Fragment } from "react"
 import { useParams } from "react-router-dom"
 
 import ProductCard from "../../components/product-card/product-card.component"
+
 import { CategoriesContext } from "../../contexts/categories.context"
 
-import "./category.styles.scss"
+import { CategoryContainer, Title } from "./category.styles"
 
 const Category = () => {
   const { category } = useParams()
   const { categoriesMap } = useContext(CategoriesContext)
   const [products, setProducts] = useState(categoriesMap[category])
 
-  // whenever category changes or categoriesMap changes, we set the value equal to categoriesMap at category
   useEffect(() => {
     setProducts(categoriesMap[category])
   }, [category, categoriesMap])
 
   return (
     <Fragment>
-      <h2 className="category-title">{category.toUpperCase()}</h2>
-      <div className="category-container">
+      <Title>{category.toUpperCase()}</Title>
+      <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
+      </CategoryContainer>
     </Fragment>
   )
 }
